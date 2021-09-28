6490c07932abf58e4142fbfd212e7e04108d236e
Sandeep Gupta <Sandeep2011069@akgec.ac.in>
2021-09-29 00:58:31 +0530

    multiply

--- practise1.java
+++ practise1.java
@@ -1,5 +1,8 @@
 public class practise1 {
 
+    static void multipy(){
+        System.out.println("Multiply");
+    }
     public static void main(String[] args) {
         System.out.println("practise 1");
         System.out.println("line 2");
